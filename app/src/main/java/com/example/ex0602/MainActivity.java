4736dd5b4db0bece1ca8505b5e3b7ea399dc37af
as8891as <as8891@bs.amalnet.k12.il>
2025-01-04 21:07:59 +0200

    Added button and button function

--- app/src/main/java/com/example/ex0602/MainActivity.java
+++ app/src/main/java/com/example/ex0602/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex0602;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +11,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button);
+    }
+
+    public void pressed(View view) {
     }
 }
